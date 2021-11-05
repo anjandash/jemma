057695a5cd1ef86abc3dae88aba983691e0a911f
anjandash <anjan8@gmail.com>
2021-11-05 11:48:12 +0100

    Added get library functions in jemma_utils

---
+++ jemma/jemma_contrib.py
@@ -0,0 +1,8 @@
+"""
+"""
+
+
+def add_metrics(metric="CMPX", metrics=target_data_df["CMPX"].tolist(), methods=target_data_df["method_id"].tolist()):
+    """
+    """
+    pass
---
+++ jemma/jemma_task_utils.py
@@ -0,0 +1,8 @@
+"""
+"""
+
+def run_models(metric="COMP", representation="TOKN", train_methods=train_method_ids, test_methods=test_method_ids, models=["MLP", "CNN", "BiLSTM"]):
+    pass
+
+def gen_representation(representation="C2VC", custom_method_text=comp_representation, method_id=method_id):
+    pass
